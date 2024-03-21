c5bb366213e23728fb352f8a380eba8e20c2a307
AlexMtzGzn <alex13141926@gmail.com>
2024-03-21 16:03:08 -0600

    Error al llamar la bibleoteca

--- src/filosofos.c
+++ src/filosofos.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <filosofos.h>
+#include "filosofos.h"
 
 void *filosofo(void * arg){
     struct Filosofo * filosofo = (void *) arg;
