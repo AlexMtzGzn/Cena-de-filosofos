c85bd38331ba80dd028a1a143295c76111f5f504
AlexMtzGzn <alex13141926@gmail.com>
2024-03-21 15:24:29 -0600

    Configurando el parametro de la struct

--- src/filosofos5.c
+++ src/filosofos5.c
@@ -2,8 +2,8 @@
 #include <pthread.h>
 #include <filosofos.h>
 
-void *filosofo(void * args){
-    
+void *filosofo(void * arg){
+    struct Filosofo * filosofo = (void *) arg;
 }
 
 void ejecutar(){
