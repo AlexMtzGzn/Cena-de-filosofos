3986438e44d1310874f39c1e20676c4d60aaeec8
AlexMtzGzn <alex13141926@gmail.com>
2024-03-21 15:28:34 -0600

    Condiciones

--- src/filosofos5.c
+++ src/filosofos5.c
@@ -9,4 +9,9 @@ void *filosofo(void * arg){
 void ejecutar(){
 
     struct Filosofo filosofos[numeroFilosofos];
+
+    for(int i  = 0; i < numeroFilosofos; i++){
+        pthread_cond_init(&condicion_tenedor[i], NULL);
+        tenedores[i] = 1;
+    }
 }
